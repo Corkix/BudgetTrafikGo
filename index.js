28a1b86a8542bc910e72a140903a48a3bc332b85
danielhaggstrom <danielhaggstrom@hotmail.se>
2022-05-12 11:34:01 +0200

    Fick fram lista

--- index.js
+++ index.js
@@ -17,11 +17,25 @@ function showOnMap(position) {
     .addTo(map);
 }
 
+async function getLongLat(long, lat) {
+  const res = await fetch(
+    `https://api.resrobot.se/v2.1/location.nearbystops?format=json&accessId=6097562d-5f91-462b-b5e9-a941856f3fd4&originCoordLat=${lat}&originCoordLong=${long}`
+  );
+  const data = await res.json();
+
+  const locationList = data.stopLocationOrCoordLocation.map((item) => {
+    return item.StopLocation.name;
+  });
+
+  console.log(locationList);
+}
+
 buttonElem.addEventListener("click", () => {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((pos) => {
       console.log(pos);
-      showOnMap(pos);
+      // showOnMap(pos);
+      getLongLat(pos.coords.longitude, pos.coords.latitude);
     });
   }
 });
